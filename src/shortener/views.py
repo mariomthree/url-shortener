ed8beeff994441912cbbb3b55ef4cba7530d320e
mariomthree <mari99manuel@gmail.com>
2021-02-15 15:05:41 +0200

    implementacao do encurtador de rota e criacao da view

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import  HttpResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'url/index.html')
+
+def create(request):
+    if request.method == 'POST':
+        link = request.POST['link']
+        unique_identifier = str(uuid.uuid4())[:5]
+        url = Url(link=link, uuid=unique_identifier)
+        url.save()
+        return HttpResponse(unique_identifier)
+
+def go(request, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect(url_details.link)
